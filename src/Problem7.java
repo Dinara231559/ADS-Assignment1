5fab7cba6b6c8ba4c2b4a465dc6933fa652e8a84
Dinara Abdukhamitova <231559@astanait.edu.kz>
2024-04-07 22:14:00 +0500

    problem 7

---
+++ src/Problem7.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Problem7 {
+    public static void reverseArray(int[] arr, int first, int last) {
+            if (first >= last) {
+                return;
+            }
+
+            int temp = arr[first];
+            arr[first] = arr[last];
+            arr[last] = temp;
+
+            reverseArray(arr, first + 1, last - 1);
+        }
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int n = scan.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scan.nextInt();
+            }
+            int first=0;
+            int last=arr.length-1;
+            reverseArray(arr, first,last);
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+
